fea1f68c879f5cde6330bcd0ff8b260b06f397db
pie <yejinh_h@naver.com>
2024-09-09 20:41:56 +0900

    refactor : schema 변경으로 인한 엔티티 변경
    - item 테이블에 price 제거
    - price 테이블에 createdAt 추가, itemType 제거 -> itemId만 들고 있도록 변경

--- src/main/java/wanted/goldroom/product/domain/item/Item.java
+++ src/main/java/wanted/goldroom/product/domain/item/Item.java
@@ -1,6 +1,8 @@
 package wanted.goldroom.product.domain.item;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -8,7 +10,7 @@ import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import wanted.goldroom.product.domain.type.Type;
 
 @Entity
 @Getter
@@ -18,17 +20,8 @@ public class Item {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
     private Type type;
-    private int salePrice;
-    private int buyPrice;
-
-    @Getter
-    @RequiredArgsConstructor
-    public enum Type {
-        GOLD_99_9_PERCENT("금 99.9%"),
-        GOLD_99_99_PERCENT("금 99.99%");
-
-        private final String product;
-    }
 }
